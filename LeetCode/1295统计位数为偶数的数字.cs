bce4bc8135e29d240f7700ad8a3900654aebae23
stark.huang <772339520@qq.com>
2020-10-26 23:52:28 +0800

    1295. 统计位数为偶数的数字

---
+++ LeetCode/1295统计位数为偶数的数字.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _1295统计位数为偶数的数字
+    {
+        public static int FindNumbers(int[] nums)
+        {
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i].ToString().Length % 2 == 0)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
