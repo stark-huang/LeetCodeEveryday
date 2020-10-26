1d0cf0b3a73ab1873491c457540f8c3b28de8c9d
stark.huang <772339520@qq.com>
2020-10-27 00:38:19 +0800

    1290. 二进制链表转整数

---
+++ LeetCode/1290二进制链表转整数.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _1290二进制链表转整数
+    {
+        public int GetDecimalValue(ListNode<int> head)
+        {
+            List<int> node = new List<int>();
+            while (head != null)
+            {
+                node.Add(head.Data);
+                head = head.Next;
+            }
+            string num = string.Join("", node);
+            return Convert.ToInt32(num, 2);
+        }
+    }
+}
---
+++ LeetCode/ListNode.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+
+    public class ListNode<T>
+    {
+        private T data;        //数据域
+        private ListNode<T> next;  //引用域
+
+        #region 构造函数
+        /// <summary>
+        /// 构造函数
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="p"></param>
+        public ListNode(T val, ListNode<T> p)
+        {
+            data = val;
+            next = p;
+        }
+        public ListNode(ListNode<T> p)
+        {
+            next = p;
+        }
+        public ListNode(T val)
+        {
+            data = val;
+            next = null;
+        }
+        public ListNode()
+        {
+            data = default(T);
+            next = null;
+        }
+        #endregion
+
+        //数据域属性
+        public T Data
+        {
+            get
+            {
+                return data;
+            }
+            set
+            {
+                data = value;
+            }
+        }
+        //引用域属性
+        public ListNode<T> Next
+        {
+            get
+            {
+                return next;
+            }
+            set
+            {
+                next = value;
+            }
+        }
+
+    }
+}
