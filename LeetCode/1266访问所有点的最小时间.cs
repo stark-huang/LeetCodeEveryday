f2bafdae5914ee5c1254cd3202c067ac754a0e7e
stark.huang <772339520@qq.com>
2020-10-26 23:48:08 +0800

    1266. 访问所有点的最小时间

---
+++ LeetCode/1266访问所有点的最小时间.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _1266访问所有点的最小时间
+    {
+        public static int MinTimeToVisitAllPoints(int[][] points)
+        {
+            int count = 0;
+            for (int i = 1; i < points.Length; i++)
+            {
+                int num1 = Math.Abs(points[i][0] - points[i - 1][0]);
+                int num2 = Math.Abs(points[i][1] - points[i - 1][1]);
+                count += num1 > num2 ? num1 : num2;
+            }
+            return count;
+        }
+    }
+}
