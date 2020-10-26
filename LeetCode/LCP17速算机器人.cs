f1e13444778b3120a865d811ee89fe28d027dc56
stark.huang <772339520@qq.com>
2020-10-27 01:00:58 +0800

    LCP 17. 速算机器人

---
+++ LeetCode/LCP17速算机器人.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LCP17速算机器人
+    {
+        public int Calculate(string s)
+        {
+            int x = 1, y = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ('A'.Equals(s[i]))
+                    x = 2 * x + y;
+                else if ('B'.Equals(s[i]))
+                    y = 2 * y + x;
+            }
+            return x + y;
+        }
+    }
+}
