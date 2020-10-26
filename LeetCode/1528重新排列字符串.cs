9043eb4bbfb1c9c03b6b3fd7fcd96ef9db5dbf4e
stark.huang <772339520@qq.com>
2020-10-27 01:11:49 +0800

    1528. 重新排列字符串

---
+++ LeetCode/1528重新排列字符串.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _1528重新排列字符串
+    {
+        public string RestoreString(string s, int[] indices)
+        {
+            Dictionary<int, string> dic = new Dictionary<int, string>();
+            for (int i = 0; i < indices.Length; i++)
+            {
+                dic.Add(indices[i], s[i].ToString());
+            }
+            var dicSort = from objDic in dic orderby objDic.Key select objDic;
+            string str = "";
+            foreach (KeyValuePair<int, string> kvp in dicSort)
+                str += kvp.Value;
+            return str;
+        }
+    }
+}
