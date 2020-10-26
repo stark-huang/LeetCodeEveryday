fd4df17c257801c5b56f9a93cfb7d207476e56b0
stark.huang <772339520@qq.com>
2020-10-27 00:19:00 +0800

    1572. 矩阵对角线元素的和

---
+++ LeetCode/1572矩阵对角线元素的和.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _1572矩阵对角线元素的和
+    {
+        public static int DiagonalSum(int[][] mat)
+        {
+            if (mat.Length == 1)
+                return mat[0][0];
+            int count = 0;
+            for (int i = 0; i < mat.Length; i++)
+            {
+                count += mat[i][i] + mat[i][mat.Length - i - 1];
+            }
+            if (mat.Length % 2 != 0)
+                count -= mat[(mat.Length / 2)][(mat.Length / 2)];
+            return count;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -10,43 +10,7 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            #region 1024视频拼接测试用例
-            /*
-            int count = 0;
-            int[][] dataArry = new int[6][];
-            dataArry[0] = new int[2] { 0, 2 };
-            dataArry[1] = new int[2] { 4, 6 };
-            dataArry[2] = new int[2] { 8, 10 };
-            dataArry[3] = new int[2] { 1, 9 };
-            dataArry[4] = new int[2] { 1, 5 };
-            dataArry[5] = new int[2] { 5, 9 };
-            _1024视频拼接.VideoStitching(dataArry, 10);
-            int[][] dataArry2 = new int[16][];
-            dataArry2[0] = new int[2] { 0, 1 };
-            dataArry2[1] = new int[2] { 6, 8 };
-            dataArry2[2] = new int[2] { 0, 2 };
-            dataArry2[3] = new int[2] { 5, 6 };
-            dataArry2[4] = new int[2] { 0, 4 };
-            dataArry2[5] = new int[2] { 0, 3 };
-            dataArry2[6] = new int[2] { 6, 7 };
-            dataArry2[7] = new int[2] { 1, 3 };
-            dataArry2[8] = new int[2] { 4, 7 };
-            dataArry2[9] = new int[2] { 1, 4 };
-            dataArry2[10] = new int[2] { 2, 5 };
-            dataArry2[11] = new int[2] { 2, 6 };
-            dataArry2[12] = new int[2] { 3, 4};
-            dataArry2[13] = new int[2] { 4, 5 };
-            dataArry2[14] = new int[2] { 5, 7 };
-            dataArry2[15] = new int[2] { 6, 9 };
-            count = _1024视频拼接.VideoStitching2(dataArry2, 9);*/
-            #endregion
-            #region 845数组中的最长山脉测试用例
-            /*
-            int[] A = new int[] { 2, 1, 4, 7, 3, 2, 5 };
-            int count = _845数组中的最长山脉.LongestMountain(A);*/
-            #endregion
-
-            int num = _7整数反转.Reverse(-123);
+            int num = 5 / 2;
         }
 
     }
