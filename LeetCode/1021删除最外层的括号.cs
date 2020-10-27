e226076d37d7b08dc5ec8e2701408dd4a9b5284b
stark.huang <772339520@qq.com>
2020-10-28 00:45:57 +0800

    1021. 删除最外层的括号

---
+++ LeetCode/1021删除最外层的括号.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class _1021删除最外层的括号
+    {
+        public string RemoveOuterParentheses(string S)
+        {
+            List<string> list = new List<string>();
+            string s = "";
+            int count = 0;
+            for (int i = 0; i < S.Length; i++)
+            {
+                if ('('.Equals(S[i]))
+                    count++;  
+                if (')'.Equals(S[i]))
+                    count--;
+                s += S[i].ToString();
+                if (count == 0)
+                {
+                    s = s.Substring(0, s.Length - 1);//去掉最后一个逗号
+                    s = s.Substring(1);//去掉第一个字
+                    list.Add(s);
+                    s = "";
+                }
+            }
+            return string.Join("", list);
+        }
+    }
+}
--- LeetCode/657机器人能否返回原点.cs
+++ LeetCode/657机器人能否返回原点.cs
@@ -36,10 +36,5 @@ namespace LeetCode
             }
             return x == 0 && y == 0;
         }
-
-        作者：chao-yuan-v
-        链接：https://leetcode-cn.com/problems/robot-return-to-origin/solution/c-ji-qi-ren-hui-dao-yuan-dian-by-chao-yuan-v/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
     }
 }
