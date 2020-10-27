ff46fadf397354484f5717a412cd9e84b52306c2
huangshifa <50603917+huangshifa@users.noreply.github.com>
2020-10-27 09:04:38 +0800

    144二叉树的前序遍历

---
+++ LeetCode/144二叉树的前序遍历.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _144二叉树的前序遍历
+    {
+        public IList<int> PreorderTraversal(TreeNode<int> root)
+        {
+            IList<int> list = new List<int>();
+            if (root == null)
+                return list;
+            var stack = new Stack<TreeNode<int>>();
+            stack.Push(root);
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();//移除顶部的节点数据
+                list.Add(node.Data);//数据加载到list中
+
+                if (node.RChild != null)
+                {
+                    stack.Push(node.RChild);//把右节点插入到stack中
+                }
+
+                if (node.LChild != null)
+                {
+                    stack.Push(node.LChild);//把左节点插入到stack中
+                }
+
+            }
+            return list;
+        }
+    }
+}
---
+++ LeetCode/TreeNode.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    /// <summary>
+    /// 二叉链表结点类
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public  class TreeNode<T>
+    {
+        private T data;               //数据域
+        private TreeNode<T> lChild;   //左孩子   树中一个结点的子树的根结点称为这个结点的孩子
+        private TreeNode<T> rChild;   //右孩子
+
+        public TreeNode(T val, TreeNode<T> lp, TreeNode<T> rp)
+        {
+            data = val;
+            lChild = lp;
+            rChild = rp;
+        }
+
+        public TreeNode(TreeNode<T> lp, TreeNode<T> rp)
+        {
+            data = default(T);
+            lChild = lp;
+            rChild = rp;
+        }
+
+        public TreeNode(T val)
+        {
+            data = val;
+            lChild = null;
+            rChild = null;
+        }
+
+        public TreeNode()
+        {
+            data = default(T);
+            lChild = null;
+            rChild = null;
+        }
+
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+
+        public TreeNode<T> LChild
+        {
+            get { return lChild; }
+            set { lChild = value; }
+        }
+
+        public TreeNode<T> RChild
+        {
+            get { return rChild; }
+            set { rChild = value; }
+        }
+}
