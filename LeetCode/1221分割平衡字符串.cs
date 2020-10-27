6607452580ac7c2508802a7aa5e73006f234fc72
huangshifa <50603917+huangshifa@users.noreply.github.com>
2020-10-27 10:28:44 +0800

    1221.分割平衡字符串

---
+++ LeetCode/1221分割平衡字符串.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class _1221分割平衡字符串
+    {
+        public int BalancedStringSplit(string s)
+        {
+            int count = 0;
+            int num = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if ('R'.Equals(s[i]))
+                    num++;
+                else if ('L'.Equals(s[i]))
+                    num--;
+                if (num == 0)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
