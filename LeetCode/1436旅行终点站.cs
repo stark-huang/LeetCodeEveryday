12de912e1dce19b429a6df7cdbe22082c09e716d
stark.huang <772339520@qq.com>
2020-10-27 21:30:48 +0800

    1436.旅行终点站

---
+++ LeetCode/1436旅行终点站.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class _1436旅行终点站
+    {
+        public string DestCity(IList<IList<string>> paths)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            for (int i = 0; i < paths.Count; i++)
+                dic.Add(paths[i][0], paths[i][1]);
+            string city = paths[0][0];
+            while (dic.ContainsKey(city))
+                city = dic[city];
+
+            return city;
+        }
+    }
+}
