2d23e59505b4fcbbb54e02c86c59559a8df55241
huangshifa <50603917+huangshifa@users.noreply.github.com>
2020-10-27 10:28:02 +0800

    1450.在既定时间做作业的学生人数

---
+++ LeetCode/1450在既定时间做作业的学生人数.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class _1450在既定时间做作业的学生人数
+    {
+        public int BusyStudent(int[] startTime, int[] endTime, int queryTime)
+        {
+            int  count =0;
+            for (int i = 0; i < startTime.Length;i++ )
+            {
+                if (startTime[i] <= queryTime && endTime[i] >= queryTime)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
