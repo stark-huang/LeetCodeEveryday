3e37b843b9b6c23b2cb4983cf58c8ba26d4fa7a6
stark.huang <772339520@qq.com>
2020-10-31 15:12:00 +0800

    804. 唯一摩尔斯密码词

---
+++ LeetCode/804唯一摩尔斯密码词.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class _804唯一摩尔斯密码词
+    {
+        public int UniqueMorseRepresentations(string[] words)
+        {
+            Dictionary<char, string> Dic = new Dictionary<char, string>(){{'a',".-"},{'b',"-..."},{'c',"-.-."},{'d',"-.."},{'e',"."},{'f',"..-."},{'g',"--."},{'h',"...."},
+            {'i',".."},{'j',".---"},{'k',"-.-"},{'l',".-.."},{'m',"--"},{'n',"-."},{'o',"---"},{'p',".--."},
+            {'q',"--.-"},{'r',".-."},{'s',"..."},{'t',"-"},{'u',"..-"},{'v',"...-"},{'w',".--"},{'x',"-..-"},
+            {'y',"-.--"},{'z',"--.."}};
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                string str = "";
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    str += Dic[words[i][j]];
+                }
+                if (!dic.ContainsKey(str))
+                {
+                    dic.Add(str, "");
+                }
+
+            }
+            return dic.Count;
+        }
+    }
+}
