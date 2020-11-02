91891378e3c82b60cb84d8846362fd6c2a088d32
huangshifa <50603917+huangshifa@users.noreply.github.com>
2020-11-02 14:37:29 +0800

    1252. 奇数值单元格的数目

---
+++ LeetCode/1252奇数值单元格的数目.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class _1252奇数值单元格的数目
+    {
+        public int OddCells(int n, int m, int[][] indices)
+        {
+            var array = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = new int[m];
+            }
+
+            for (int i = 0; i < indices.Length; i++)
+            {
+                for (int row = 0; row < n; row++)
+                {
+                    array[row][indices[i][1]]++;
+                }
+                for (int cell = 0; cell < m; cell++)
+                {
+                    array[indices[i][0]][cell]++;
+                }
+            }
+            int count = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                count += array[i].Where(x => x % 2 == 1).Count();
+                //count += array[i].Select(x => x % 2 == 1).Count();//错误的方式
+            }
+
+            return m * n - count;
+        }
+    }
+}
--- LeetCode/381O(1)时间插入删除和获取随机元素允许重复.cs
+++ LeetCode/381O(1)时间插入删除和获取随机元素允许重复.cs
@@ -8,70 +8,84 @@ namespace LeetCode
 {
     class _381O_1_时间插入删除和获取随机元素允许重复
     {
-        // 字典内使用 HashSet 是为了实现 O(1) 删除索引，而且索引不会重复
-        Dictionary<int, HashSet<int>> IndexDictionary { get; set; } = new Dictionary<int, HashSet<int>>();
-        List<int> Values { get; set; } = new List<int>();
-
+        //主要为add方法，时间复杂度为O(1)，需要扩容时为O(n)，因此该操作平均时间复杂度为O(1).
+        private List<int> _LIST;
+        private int _LENGTH; //当前集合长度
+        //字典主要用到Add(时间复杂度同List.Add)、CntainsKey和dic[index]，时间复杂度均为O(1)，hashset操作均为O(1)
+        Dictionary<int, HashSet<int>> _DICINDEX;
         /** Initialize your data structure here. */
         public _381O_1_时间插入删除和获取随机元素允许重复()
         {
-            /* 列表随机插入是 0(n)，因为要后移插入位置之后的元素，但追加在列表尾部是O(1)
-             * 列表按索引查找是 O(1)，按值查找是 O(n)
-             */
-
+            _LIST = new List<int>();
+            _LENGTH = 0;
+            _DICINDEX = new Dictionary<int, HashSet<int>>();
         }
 
         /** Inserts a value to the collection. Returns true if the collection did not already contain the specified element. */
         public bool Insert(int val)
         {
-            // 使用一个字典维护某个数字的所有索引列表
-            if (!IndexDictionary.TryGetValue(val, out var indexSet))
+            //由于删除集合元素是覆盖，用额外变量维护集合的实际有效长度，当集合元素数量超过有效长度时，
+            //新增元素从有效长度的末尾元素开始将无效的元素替换，反之，才添加到末尾
+            if (_LIST.Count > _LENGTH)
+                _LIST[_LENGTH] = val;
+            else
+                _LIST.Add(val);
+            _LENGTH++;
+            bool isExists = false;
+            //用字典里的当前插入值对应的HashSet保存插入后该元素的索引，若对应索引数大于0，则表示已存在
+            if (_DICINDEX.ContainsKey(val))
             {
-                indexSet = new HashSet<int>();
-                IndexDictionary.Add(val, indexSet);
+                isExists = _DICINDEX[val].Count > 0;
+                _DICINDEX[val].Add(_LENGTH - 1);
             }
-
-            Values.Add(val);
-            indexSet.Add(Values.Count - 1);
-            return true;
+            else
+            {
+                _DICINDEX.Add(val, new HashSet<int>() { _LENGTH - 1 });
+            }
+            return !isExists;
         }
 
         /** Removes a value from the collection. Returns true if the collection contained the specified element. */
         public bool Remove(int val)
         {
-            // 字典不存在某个数字的索引，也就不存在某个数字
-            if (!IndexDictionary.TryGetValue(val, out var valIndexList) ||
-                valIndexList.Count == 0)
-                return false;
-
-            var lastValue = Values.Last();
-            if (lastValue == val)
+            int index = -1;
+            if (_DICINDEX.ContainsKey(val) && _DICINDEX[val].Count > 0)
             {
-                // 直接在列表最后移除
-                valIndexList.Remove(Values.Count - 1);
-                Values.RemoveAt(Values.Count - 1);
+                //获取删除元素的索引
+                index = _DICINDEX[val].FirstOrDefault();
+                //从字典中删除该索引
+                _DICINDEX[val].Remove(index);
+            }
+            //没有找到，返回false，无需删除
+            if (index == -1) return false;
+            //只有一个时，即待删除元素索引等于末尾元素索引时，无需替换，因已删除过该元素的索引，只需维护集合长度即可
+            if (index == _LENGTH - 1)
+            {
+                _LENGTH--;
                 return true;
             }
+            //若有多个元素时，末尾元素覆盖待删除元素后
+            _LIST[index] = _LIST[_LENGTH - 1];
+            _LENGTH--;
+            //需要维度覆盖后的也就是末尾元素的索引，将旧索引删除，然后保存当前索引即可
+            _DICINDEX[_LIST[index]].Remove(_LENGTH);
+            _DICINDEX[_LIST[index]].Add(index);
 
-            // 将Values最后一个元素的值替换要删除的元素的最后一个索引的位置，然后Values删除最后一个元素，即为0(1)删除
-            // 被替换的值的索引列表应该移除其索引值（Values的长度-1），并追加一个被删除元素所在的索引值（因为被替换的值被替换到这个索引了）
-            var valLastIndex = valIndexList.Last();
-            var exchangeIndexList = IndexDictionary[lastValue];
-
-            valIndexList.Remove(valLastIndex);
-            exchangeIndexList.Remove(Values.Count - 1);
-            exchangeIndexList.Add(valLastIndex);
-
-            Values[valLastIndex] = lastValue;
-            Values.RemoveAt(Values.Count - 1);
             return true;
         }
 
         /** Get a random element from the collection. */
         public int GetRandom()
         {
-            return Values[new Random().Next(0, Values.Count)];
+            int res = -1;
+            int seed = Guid.NewGuid().GetHashCode();
+            Random rd = new Random(seed);
+            int idx = rd.Next(_LENGTH);
+            if (idx >= 0 && _LIST.Count > 0)
+                res = _LIST[idx];
+            return res;
         }
+
 
     }
 }
