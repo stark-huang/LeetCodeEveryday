dfa0f9be2ce3b3102369cfa36abffc40baff3ac3
huangshifa <50603917+huangshifa@users.noreply.github.com>
2020-11-02 13:43:15 +0800

    832.翻转图像

---
+++ LeetCode/832翻转图像.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class _832翻转图像
+    {
+        public int[][] FlipAndInvertImage(int[][] A)
+        {
+            for (int i = 0; i < A.Length; i++)
+            {
+                Array.Reverse(A[i]);
+                for (int j = 0; j < A[i].Length; j++)
+                    if (A[i][j] == 0) A[i][j] = 1;
+                    else A[i][j] = 0;
+            }
+            return A;
+        }
+    }
+}
