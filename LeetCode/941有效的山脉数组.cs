4de04a4677a05b5791ceee3727fd44c630207bfe
huangshifa <50603917+huangshifa@users.noreply.github.com>
2020-11-03 14:56:08 +0800

    941. 有效的山脉数组

---
+++ LeetCode/941有效的山脉数组.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class _941有效的山脉数组
+    {
+        public bool ValidMountainArray(int[] A)
+        {
+            if (A.Length < 3) return false;
+            int tcout = 0;
+            int acout = 0;
+            for (int i = 1; i < A.Length - 1; ++i)
+            {
+                if (A[i - 1] < A[i] && A[i] > A[i + 1]) ++tcout;
+                else if (A[i - 1] > A[i] && A[i] < A[i + 1]) ++acout;
+                else if (A[i - 1] == A[i] || A[i] == A[i + 1]) return false;
+            }
+            return tcout == 1 && acout == 0;
+        }
+    }
+}
