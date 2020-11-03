3b6365de592b0c9556129925081b56f83a7e8cda
huangshifa <50603917+huangshifa@users.noreply.github.com>
2020-11-03 15:12:39 +0800

    1323. 6 和 9 组成的最大数字

---
+++ LeetCode/1323(6和9组成的最大数字).cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class _1323_6和9组成的最大数字_
+    {
+        public int Maximum69Number(int num)
+        {
+            string str = num.ToString();
+            if (str.Contains("6"))
+            {
+                int index = str.IndexOf("6");
+                str = str.Remove(index, 1);
+                str = str.Insert(index, "9");
+            }
+            return Convert.ToInt32(str);
+        }
+    }
+}
