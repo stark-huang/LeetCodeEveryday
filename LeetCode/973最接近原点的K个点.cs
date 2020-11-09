cb6839869ed34363d5252bc42a795fb35514e476
huangshifa <50603917+huangshifa@users.noreply.github.com>
2020-11-09 11:09:09 +0800

    973. 最接近原点的 K 个点

---
+++ LeetCode/973最接近原点的K个点.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    class _973最接近原点的K个点
+    {
+        public int[][] KClosest(int[][] points, int K)
+        {
+            int[][] nums = new int[K][];
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+            for (int i = 0; i < points.Length; i++)
+            {
+                dic.Add(i, (points[i][0] * points[i][0] + points[i][1] * points[i][1]));
+            }
+            Dictionary<int, int> dic1_SortedByKey = dic.OrderBy(p => p.Value).ToDictionary(p => p.Key, o => o.Value);
+
+            foreach (KeyValuePair<int, int> item in dic1_SortedByKey)
+            {
+                if (K > 0)
+                {
+                    nums[K - 1] = points[item.Key];
+                    K--;
+                }
+                else
+                {
+                    return nums;
+                }
+            }
+            return nums;
+        }
+    }
+}
